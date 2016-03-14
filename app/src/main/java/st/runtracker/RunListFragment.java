f07d08f3d7f1ed3309d9469c178dacb10a49f591
Teng Sun <tengsun@cn.ibm.com>
2016-03-14 14:57:50 +0800

    add run menu create item to add new record

--- app/src/main/java/st/runtracker/RunListFragment.java
+++ app/src/main/java/st/runtracker/RunListFragment.java
@@ -1,10 +1,14 @@
 package st.runtracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
@@ -20,15 +24,45 @@ import st.runtracker.model.Run;
 public class RunListFragment extends ListFragment {
 
     private RunDatabaseHelper.RunCursor cursor;
+    private static final int REQUEST_NEW_RUN = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        // query the run list from database
         cursor = RunManager.getRunManager(getActivity()).queryRuns();
 
         // create an adapter to point at this cursor
         RunCursorAdapter adapter = new RunCursorAdapter(getActivity(), cursor);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.run_list_options, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.tracker_menu_new_run:
+                Intent i = new Intent(getActivity(), RunActivity.class);
+                startActivityForResult(i, REQUEST_NEW_RUN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (REQUEST_NEW_RUN == requestCode) {
+            cursor.requery();
+            ((RunCursorAdapter) getListAdapter()).notifyDataSetChanged();
+        }
     }
 
     @Override
